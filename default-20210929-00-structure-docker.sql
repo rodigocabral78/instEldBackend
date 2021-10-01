4182c45601e3b3388ca4cf9e9afddd58734e346c
Rodrigo Cabral <rodrixcornell@gmail.com>
2021-10-01 08:18:28 -0400

    No ramo main
    * Mudanças a serem submetidas:
    	new file:   .gitattributes
    	new file:   default-20210929-00-structure-docker.sql
    	new file:   default-20210929-01-data-docker.sql
    
    @rodrixcornell Rodrigo Cabral <rodrixcornell@gmail.com>

---
+++ default-20210929-00-structure-docker.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b30f0b2461f1263b7a357898672379770cf7f610af78cd6aa66df24f9c220a4e
+size 2579
---
+++ default-20210929-01-data-docker.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b7cfd65589642bd7c739d0cf9a0646f2c9cdbcbf90115375b9f58eda9cd341d
+size 1419
